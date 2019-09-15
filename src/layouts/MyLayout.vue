de5d575111fc94f94c85baed06a4b13d8527b751
Shakhruz Ashirov <shakhruz@gmail.com>
2019-09-15 23:09:44 +0500

    первая версия, наброски на структуру страниц

---
+++ src/layouts/MyLayout.vue
@@ -0,0 +1,34 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-toolbar-title class="absolute-center">
+          MinterX
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { openURL } from 'quasar'
+
+export default {
+  name: 'MyLayout',
+  data () {
+    return {
+      leftDrawerOpen: false
+    }
+  },
+  methods: {
+    openURL
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,63 @@
+<template>
+  <q-page class="flex">
+    <div id="calculator">
+      <h3>Обмен Валют</h3>
+      <div>Шаг 1 из 2. Выберите валюту, сумму и направление обмена:</div>
+      <div class="q-pa-md" style="max-width: 300px">
+        <div class="q-gutter-md">
+          <q-select outlined v-model="model" :options="options" label="Outlined" />
+          <q-btn outline color="primary" icon="compare_arrows" />
+          <q-select outlined v-model="model" :options="options" label="Outlined" />
+          <q-input outlined v-model="text" label="Outlined" />
+          <q-input outlined v-model="text" label="Outlined" />
+          <q-btn outline color="primary" label="Далее" />
+        </div>
+      </div>
+    </div>
+    <div id="exhange_rates" class="q-pa-md" style="max-width: 300px">
+      <div class="q-gutter-md">
+        <h3>Обменные курсы</h3>
+        <table>
+          <thead>
+            <tr><td></td><td>BIP</td><td>BTC</td></tr>
+            <tr><td>Продажа</td>1<td>0.0487</td><td>472сатоши</td></tr>
+            <tr><td>Покупка</td>1<td>0.0450</td><td>434сатоши</td></tr>
+            <tr><td>Резерв</td><td>50000</td><td>0.1</td></tr>
+          </thead>
+        </table>
+      </div>
+    </div>
+    <div id="history" class="q-pa-md" style="max-width:300px">
+      <div class="q-gutter-md">
+        <h3>История операций</h3>
+        <p>Кол-во операций всего/сегодня: 100/1</p>
+        <table>
+          <tr>
+            <td>Время</td>
+            <td>Продажа</td>
+            <td>Транзакция продажи</td>
+            <td>Покупка</td>
+            <td>Транзакция покупки</td>
+            <td>Статус</td>
+          </tr>
+          <tr>
+            <td>15.09 16:25</td>
+            <td>110 BIP</td>
+            <td></td>
+            <td>Покупка</td>
+            <td>Транзакция покупки</td>
+            <td>Статус</td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+</style>
